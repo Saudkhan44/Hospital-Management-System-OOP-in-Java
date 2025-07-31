dc1ef2ba1a0e93fc9626e053d288526eac53eda8
Saudkhan44 <msaudkhan400@gmail.com>
2025-07-31 00:14:59 -0700

    Deleting Extra Registration file for Patients

--- src/newHospital/Main.java
+++ src/newHospital/Main.java
@@ -6,7 +6,7 @@ public class Main {
         HospitalData.patientList.add(new Patient("Saud","saud400@gmail.com","1234567890",
                 "Male",21, "12345"));
         HospitalData.patientList.add(new Patient("Ali","ali400@gmail.com","1234567890",
-                "Male",50, "12345"));
+                "Male",30, "12345"));
         HospitalData.doctorList.add(new Doctor("Saqib","saqib400@gmail.com","1234567890",
                 "Male","Medicine", "12345"));
         HospitalData.doctorList.add(new Doctor("Amjad","amjad400@gmail.com","1234567890",
